80a3fb80b5d00e2e26e47fe5e214f2c9b57f8443
gogym <34082822+gogym@users.noreply.github.com>
2020-03-23 16:20:48 +0800

    .

---
+++ gim-example/gim-server-test/src/main/java/com/gettyio/gim/server/Server2.java
@@ -0,0 +1,51 @@
+package com.gettyio.gim.server;/*
+ * 类名：server
+ * 版权：Copyright by www.getty.com
+ * 描述：
+ * 修改人：gogym
+ * 时间：2019/12/24
+ */
+
+import com.gettyio.gim.GimStarter;
+import com.gettyio.gim.cluster.redis.RedisProperties;
+import com.gettyio.gim.intf.OfflineMsgIntf;
+
+public class Server2 {
+
+
+    public static void main(String[] args) {
+
+        RedisProperties redisProperties = new RedisProperties();
+        redisProperties.setHost("192.168.167.111");
+        redisProperties.setPort(6379);
+        redisProperties.setPassword("inhand@redis2017");
+
+
+        GimConfig gimConfig = new GimConfig();
+        //端口号不同，用于测试集群
+        gimConfig.port(4568);
+
+        gimConfig.enableOffline(true);
+        gimConfig.cluster(true, "one", redisProperties);
+
+        GimStarter gimStarter = new GimStarter(gimConfig);
+
+        try {
+            gimStarter.start(new GimStarter.OnStartListener() {
+                @Override
+                public void onStart(GimContext gimContext) {
+                    gimContext.offlineMsgIntf(new OfflineMsgIntf() {
+                        @Override
+                        public void offlineMsg(String msg) {
+                            System.out.println("来了个离线消息:" + msg);
+                        }
+                    });
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
